41c96b307669e3955f5a62d004d3733849a7b504
MB-29 <55185203+MB-29@users.noreply.github.com>
2021-11-19 10:55:51 +0100

    criteria parallel

--- criteria_parallel.py
+++ criteria_parallel.py
@@ -9,11 +9,11 @@ import argparse
 from utils import generate_random_A
 from discrete_controller import DiscreteController
 
-T0 = 20
+T0 = 100
 n_samples = 500
-gamma = 1
+gamma = 10
 sigma = 1
-n_gradient = 100
+n_gradient = 300
 batch_size = 100
 d = 4
 B = torch.eye(d)
@@ -29,25 +29,30 @@ learning_rate = 0.1
 
 if __name__ == '__main__':
     # arg_parser = argparse.ArgumentParser()
-    # arg_parser.add_argument('task_id', type=int)
+    # arg_parser.add_argument('task_id', type=int)  
     # arg_parser.add_argument('-o', '--optimality', type=str, default='D')
     # arg_parser.add_argument('-c', '--certainty', action='store_true', default=False)
     # args = arg_parser.parse_args()
     # optimality = args.optimality
     # task_id = args.task_id
-    # certainty = args.certainty        
+    # certainty = args.certainty    
+    #  
     task_id = int(sys.argv[1])
-    # arg_1 = int(str(task_id)[0])
-    # arg_2 = int(str(task_id)[1])
-    # optimality = ['A', 'D', 'E', 'L', 'T'][arg_1-1]
-    optimality = 'E'
-    stochastic = (task_id%2 == 0)
+
+    arg_1 = int(str(task_id)[0])
+    arg_2 = int(str(task_id)[1])
+    optimality = ['A', 'D', 'E', 'L', 'T'][arg_1-1]
+    stochastic = (arg_2%2 == 0)
+    
+    # optimality = 'E'
+    # stochastic = (task_id%2 == 0)
+
 
     print(f'{n_samples} samples, task {task_id}, optimality {optimality}, stochastic={stochastic}')
     print(f'gamma {gamma}, sigma={sigma}')
 
     output = {}
-    loss_values = np.zeros((n_samples, n_gradient))
+    loss_values = np.zeros((n_samples, n_gradient, 2))
     error_values = np.zeros((n_samples, n_gradient))
     for sample_index in tqdm(range(n_samples)):
         print(f'sample {sample_index}')
@@ -62,7 +67,7 @@ if __name__ == '__main__':
             stochastic=stochastic,
             test=True
         )
-        loss_values[sample_index, :] = sample_loss
+        loss_values[sample_index, :, :] = np.array(sample_loss)
         error_values[sample_index, :] = sample_error
 
     output['loss'] = loss_values
@@ -71,6 +76,7 @@ if __name__ == '__main__':
     output['stochastic'] = stochastic
     output['gamma'] = gamma
     output['sigma'] = sigma
+    output['T0'] = T0
     
 
 
